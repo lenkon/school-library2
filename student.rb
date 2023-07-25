2e9d4bef87754938ea5a12b392000c041a46871f
lenkon <inoulenkon@gmail.com>
2023-07-25 19:01:26 +0530

    Create student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require_relative person
+
+class Student < Person
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: true)
+    @classroom = classroom
+    @parent_permission = parent_permission
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
+end
