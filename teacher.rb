29188d5b33742d484742e67147b5c0cc8960aee0
lenkon <inoulenkon@gmail.com>
2023-07-25 19:17:48 +0530

    Create teacher class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require_relative person
+
+class Teacher < Person
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: true)
+    @specialization = specialization
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services?
+    true
+  end
+end
