de01f9793c629e991451a2d6d5c749baefcdf593
lenkon <inoulenkon@gmail.com>
2023-07-27 11:19:56 +0530

    Create has-many relationship between Book and Rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,9 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :person, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    @book = book
   end
 end
