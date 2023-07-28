0e855e954ff7c1d57e3b56cbadcb6b43b16e3d50
lenkon <inoulenkon@gmail.com>
2023-07-28 11:09:49 +0530

    Create App class and define methods to show user options

---
+++ app.rb
@@ -0,0 +1,42 @@
+require_relative 'student'
+require_relative 'teacher'
+require_relative 'book'
+require_relative 'rental'
+
+class App
+
+  def initialize
+    @books = []
+    @rentals = []
+    @people = []
+  end
+
+  def run
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
+  
+  OPTIONS = {
+    1 => :list_books,
+    2 => :list_people,
+    3 => :create_person,
+    4 => :create_book,
+    5 => :create_rental,
+    6 => :list_rentals,    
+  }.freeze
+  
+  def option(option)
+    method_name = OPTIONS[option]
+    if method_name
+      send(method_name)
+    else
+      puts 'Invalid option, please type correct number!'
+    end
+  end  
+end
--- main.rb
+++ main.rb
@@ -2,11 +2,17 @@ require_relative 'app'
 
 def main
   app = App.new
+  puts 'Welcome to School Library App!'
   loop do
     app.run
     option = gets.chomp.to_i
+    if option === 7
+      puts 'Thank you for using this app!'
+      puts
+      break      
+    end
     app.option(option)
-  end
+  end  
 end
 
 main
